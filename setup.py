cc447397151befa021241150550e83831f35a48e
Lauri Tuominen <lauri@Lauri-MacBook-Pro-3.local>
2020-12-23 15:59:05 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Functional Python Sequence",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/lauriat/func",
+    url="https://github.com/lauriat/funct",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
